2811a98344ca01868aa85dd24e251b263487dc14
zbahrani <139264392+zbahrani@users.noreply.github.com>
2024-08-17 18:01:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from keras.layers import Dense
 
 (train_image, train_label), (test_image, test_label) = datasets.cifar10.load_data()
 
-print(train_image.shape)
+# print(train_image.shape)
 
-plt.imshow(train_image[0])
-plt.show()
+# plt.imshow(train_image[0])
+# plt.show()
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+print(class_names[train_label[0][0]])
