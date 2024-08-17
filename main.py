fc7fab2ffbed2a52c6c26f2475dc3961ecac433e
zbahrani <139264392+zbahrani@users.noreply.github.com>
2024-08-17 17:49:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
 
 from keras import datasets
 from keras.layers import Dense
 
 (train_image, train_label), (test_image, test_label) = datasets.cifar10.load_data()
+
 print(train_image.shape)
+
+plt.imshow(train_image[0])
+plt.show()
