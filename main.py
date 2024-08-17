48130bf5851248ac11d9a9b984380957901d3688
zbahrani <139264392+zbahrani@users.noreply.github.com>
2024-08-17 21:10:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-
+from keras.models import Sequential
 from keras import datasets
-from keras.layers import Dense
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 
 (train_image, train_label), (test_image, test_label) = datasets.cifar10.load_data()
 
@@ -15,4 +15,20 @@ from keras.layers import Dense
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
-print(class_names[train_label[0][0]])
+# print(class_names[train_label[0][0]])
+
+model = Sequential()
+model.add(Conv2D(32 , kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10))
+
+model.compile(optimizer='adam', 
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+h = model.fit(train_image, train_label,
+              epochs=10, validation_data=(test_image, test_label))
